daa542a09c987b51c90331ff6fd1969b30151edb
Ayoub Lahyaoui <ayoub.lahyaoui@hotmail.com>
2026-02-06 08:27:16 +0100

    feat: Move literal values to variables

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import socket
 
 from server import MyServer
 
-SSH_KEY_PATH = '.ssh/server.key'
+IP_ADDR = '0.0.0.0'
 SOCKET_PORT = 6767
+CONNEXION_TIMEOUT = 60
+SSH_KEY_PATH = '.ssh/server.key'
 
 def run_server():
     # 2. Configuration du socket réseau
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('0.0.0.0', SOCKET_PORT))
+    server_socket.bind((IP_ADDR, SOCKET_PORT))
     server_socket.listen(5)
     
     print(f"[*] Serveur en attente sur le port {SOCKET_PORT}...")
@@ -21,7 +23,7 @@ def run_server():
     # 3. Mise en place du transport SSH
     transport = paramiko.Transport(client)
     
-    # Chargement de la clé d'hôte (génère-la avec : ssh-keygen -t rsa -f server.key)
+    # Chargement de la clé d'hôte
     host_key = paramiko.RSAKey.from_private_key_file(SSH_KEY_PATH)
     transport.add_server_key(host_key)
 
@@ -29,7 +31,7 @@ def run_server():
     transport.start_server(server=server)
 
     # 4. Attente d'un canal
-    chan = transport.accept(20)
+    chan = transport.accept(CONNEXION_TIMEOUT)
     if chan is None:
         print("*** Pas de canal ouvert.")
         return
